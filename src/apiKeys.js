bd5ce75370f5a9a19ecbcc3e0801c31adfb893dd
vibhor2310 <95543808+vibhor2310@users.noreply.github.com>
2023-07-15 11:34:51 +0530

    Update apiKeys.js

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,5 +1,5 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
 module.exports = {
-  key: "{Your API Key Here}",
+  key: "{c0b3d62b91eac91243623bdef951f811}",
   base: "https://api.openweathermap.org/data/2.5/",
 };
